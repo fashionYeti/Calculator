9f4289c9ee3eb3806da2353733e9a209b5d95741
iDimon <macbookair@iDimon.local>
2015-04-19 15:02:01 +0300

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by iDimon on 19.04.15.
+//  Copyright (c) 2015 ___iDimon___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by iDimon on 19.04.15.
+//  Copyright (c) 2015 ___iDimon___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
